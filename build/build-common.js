ca97a0defff27d317cc040525f07a6e9899d8361
NE-SmallTown <ne_smalltown@163.com>
2021-02-25 22:12:07 +0800

    feat: update work.json && add hover ui

--- build/build-common.js
+++ build/build-common.js
@@ -5,10 +5,12 @@ const wordArr = require('../data/word');
 
 const PATH_COMMON_DATA_DIR = path.resolve(__dirname, '../data-common');
 
-fs.mkdirSync(path.resolve(PATH_COMMON_DATA_DIR));
+try {
+  fs.mkdirSync(path.resolve(PATH_COMMON_DATA_DIR));
+} catch (e) {}
 
 const commonWordArr = wordArr.filter(wordItem => {
-  if (wordItem.strokes > 10) {
+  if (wordItem.strokes > 10 || wordItem.more.startsWith('搜索与')) {
     return false;
   }
 
